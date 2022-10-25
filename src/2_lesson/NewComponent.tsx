793fccc302e7b4e098eef4192a01428e9d9da1d0
genamontana <predator199@yandex.ru>
2022-10-25 10:09:24 +0300

    map complied

--- src/2_lesson/NewComponent.tsx
+++ src/2_lesson/NewComponent.tsx
@@ -1,24 +1,34 @@
-type NewComponentType = {
+/*type NewComponentType = {
     students: Array<StudentType>
 }
 type StudentType = {
     id: number,
     name: string,
     age: number
+}*/
+
+type TopCarsType = {
+    manufacturer: string,
+    model: string
 }
 
 
-export const NewComponent = (props: NewComponentType) => {
+export const NewComponent = () => {
+    const topCars = [
+        {manufacturer: 'BMW', model: 'm5cs'},
+        {manufacturer: 'Mercedes', model: 'e63s'},
+        {manufacturer: 'Audi', model: 'rs6'}
+    ]
     return (
-        <ul>
-            {props.students.map((objectFromStudentArray, index) => {
+        <table>
+            {topCars.map((topCars, index) => {
                 return (
-                    <li key={objectFromStudentArray.id}>
-                        <span>{objectFromStudentArray.name}</span>
-                        <span> age: {objectFromStudentArray.age}</span>
-                    </li>
+                    <tr>
+                        <td><span>{topCars.manufacturer}</span></td>
+                        <td><span> {topCars.model}</span></td>
+                    </tr>
                 )
             })}
-        </ul>
+        </table>
     )
 }
